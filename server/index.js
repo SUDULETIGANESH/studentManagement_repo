3205924d9f6acd74aea59ba14c5cfae63b0d6239
Ganesh <ganesh.sudileti@gmail.com>
2025-07-01 23:51:53 +0530

    added env file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import studentRoutes from './Routes/studentRoute.js';
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express()
-const port = 3000;
-
+const port = process.env.PORT || 3000;
 app.use(express.json())
 app.use(cors())
 
